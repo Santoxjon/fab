ab43d30e3cc793e8763511d0c02522539854e143
Jon Santos <jon.santos@cactussoft.biz>
2023-12-03 21:36:51 +0100

    SSL enabled and puppeteer config updated

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 const express = require('express');
 require('dotenv').config();
+
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const scheduler = require('./schedulers/nextMatch.scheduler');
-// const fs = require('fs');
-// const https = require('https');
-// const debug = require('debug')('fab:server');
-// const axios = require('axios');
+const fs = require('fs');
+const https = require('https');
+const debug = require('debug')('fab:server');
+const axios = require('axios');
 
 const app = express();
-// let credentials = null;
-// try {
-//   const privateKey = fs.readFileSync('ssl/privkey1.pem', 'utf8');
-//   const certificate = fs.readFileSync('ssl/cert1.pem', 'utf8');
-//   const ca = fs.readFileSync('ssl/chain1.pem', 'utf8');
+let credentials = null;
+try {
+  const privateKey = fs.readFileSync('ssl/privkey1.pem', 'utf8');
+  const certificate = fs.readFileSync('ssl/cert1.pem', 'utf8');
+  const ca = fs.readFileSync('ssl/chain1.pem', 'utf8');
 
-//   credentials = {
-//     key: privateKey,
-//     cert: certificate,
-//     ca,
-//   };
-// } catch (err) {
-//   console.error('Error reading file:', err);
-// }
+  credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca,
+  };
+} catch (err) {
+  console.error('Error reading file:', err);
+}
 
-// const httpsServer = https.createServer(credentials, app);
-// httpsServer.listen(443, () => {
-//   console.log('HTTPS Server running on port 443');
-// });
-// const onListening = () => {
-//   const addr = httpsServer.address();
-//   const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-//   debug('Listening on ' + bind);
-// };
-// httpsServer.on('listening', onListening);
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
+const onListening = () => {
+  const addr = httpsServer.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+};
+httpsServer.on('listening', onListening);
 console.log(process.cwd());
 
 const indexRouter = require('./routes/index');
@@ -52,30 +53,30 @@ const swaggerOptions = {
   customCss: '.swagger-ui .topbar { display: none }', // Hide the top bar containing the "Authorize" button
 };
 
-// const IP2LOCATION_API_URL = process.env.IP2LOCATION_API_URL;
-// const IP2LOCATION_API_TOKEN = process.env.IP2LOCATION_API_TOKEN;
+const IP2LOCATION_API_URL = process.env.IP2LOCATION_API_URL;
+const IP2LOCATION_API_TOKEN = process.env.IP2LOCATION_API_TOKEN;
 
-// app.use((req, res, next) => {
-//   try {
-//     if (req.ip === '::1') return next(); // Skip if localhost
-//     const clientIp = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0];
-//     axios
-//       .get(
-//         `${IP2LOCATION_API_URL}/?key=${IP2LOCATION_API_TOKEN}&ip=${clientIp}&format=json`
-//       )
-//       .then((response) => {
-//         console.log(
-//           'Conexión desde:',
-//           response.data.ip,
-//           'en',
-//           response.data.city_name
-//         );
-//       });
-//   } catch (err) {
-//     console.error('Error getting client IP:', err);
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  try {
+    if (req.ip === '::1') return next(); // Skip if localhost
+    const clientIp = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0];
+    axios
+      .get(
+        `${IP2LOCATION_API_URL}/?key=${IP2LOCATION_API_TOKEN}&ip=${clientIp}&format=json`
+      )
+      .then((response) => {
+        console.log(
+          'Conexión desde:',
+          response.data.ip,
+          'en',
+          response.data.city_name
+        );
+      });
+  } catch (err) {
+    console.error('Error getting client IP:', err);
+  }
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/matches', matchesRouter);
--- helpers/puppeteer.helper.js
+++ helpers/puppeteer.helper.js
@@ -1,13 +1,19 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const cache = require('../config/cache');
+require('dotenv').config();
+
+const BROWSER_OPTIONS = {
+  headless: 'new',
+  ...(process.env.NODE_ENV === 'production' && {
+    executablePath: '/usr/bin/google-chrome',
+  }),
+};
 
 const getDom = (url) => {
   return new Promise((resolve, reject) => {
     puppeteer
-      .launch({
-        headless: 'new',
-      })
+      .launch(BROWSER_OPTIONS)
       .then((browser) => {
         return browser.newPage();
       })
